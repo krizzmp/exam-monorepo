08a9307dfa3ab2f000b9ed3004dd084a11e7a26a
kristofferpetersen <kp@morningtrain.dk>
2019-11-30 23:09:39 +0100

    add typeorm sql.js database

--- server/src/createApolloServer.ts
+++ server/src/createApolloServer.ts
@@ -1,38 +1,57 @@
-// import {createConnection} from "typeorm";
+import {
+  createConnection,
+  Entity,
+  getRepository,
+  PrimaryColumn
+} from "typeorm";
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema, Field, ObjectType } from "type-graphql";
-import { Arg, Int, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import * as path from "path";
+
 @ObjectType()
+@Entity()
 export class LineItem {
   @Field()
+  @PrimaryColumn()
   id: string = "hello";
 }
-@Resolver()
-export class LineItemResolver {
-  @Query(() => [LineItem]) lineItems() {
-    return [new LineItem()];
-  }
-  @Query(() => LineItem) lineItem(@Arg("id") id: string) {
-    return new LineItem();
-  }
-  @Mutation()
-  scanLineItem(@Arg("itemId") itemId: string): boolean {
-    return true;
-  }
-}
+
 export async function createApolloServer() {
-  // await createConnection();
+  await createConnection({
+    type: "sqljs",
+    entities: [LineItem],
+    synchronize: true
+  });
+
+  @Resolver()
+  class LineItemResolver {
+    repository = getRepository(LineItem);
+    @Query(() => [LineItem]) lineItems() {
+      return this.repository.find();
+    }
+    @Query(() => LineItem) lineItem(@Arg("id") id: string) {
+      return this.repository.findOne(id);
+    }
+    @Mutation()
+    scanLineItem(@Arg("itemId") itemId: string): boolean {
+      const t = new LineItem();
+      t.id = itemId;
+      this.repository.save(t);
+      return true;
+    }
+  }
 
   return new ApolloServer({
     schema: await buildSchema({
       emitSchemaFile: {
-        path: path.resolve(__dirname, "../", "schema.graphql")
+        path: path.resolve(__dirname, "../schema.graphql")
       },
       resolvers: [LineItemResolver]
     }),
     context: ({ req, res }) => ({ req, res })
   });
 }
+
 export const apolloServer = createApolloServer();
--- server/src/index.test.ts
+++ server/src/index.test.ts
@@ -2,6 +2,19 @@ import { apolloServer, LineItem } from "./createApolloServer";
 import { gql } from "apollo-server-express";
 import { createTestClient } from "apollo-server-testing";
 
+it("should scan LineItem", async function() {
+  const { mutate } = createTestClient(await apolloServer);
+  const response = await mutate({
+    mutation: gql`
+      mutation($itemId: String!) {
+        scanLineItem(itemId: $itemId)
+      }
+    `,
+    variables: { itemId: "string" }
+  });
+  expect(response.data.scanLineItem).toBe(true);
+});
+
 it("should query LineItems", async function() {
   const { query } = createTestClient(await apolloServer);
   const response = await query({
@@ -13,7 +26,7 @@ it("should query LineItems", async function() {
       }
     `
   });
-  expect(response.data.lineItems).toContainEqual<LineItem>({ id: "hello" });
+  expect(response.data.lineItems).toContainEqual<LineItem>({ id: "string" });
 });
 it("should query LineItem", async function() {
   const { query } = createTestClient(await apolloServer);
@@ -27,17 +40,5 @@ it("should query LineItem", async function() {
     `,
     variables: { itemId: "string" }
   });
-  expect(response.data.lineItem).toEqual<LineItem>({ id: "hello" });
-});
-it("should scan LineItem", async function() {
-  const { mutate } = createTestClient(await apolloServer);
-  const response = await mutate({
-    mutation: gql`
-      mutation($itemId: String!) {
-        scanLineItem(itemId: $itemId)
-      }
-    `,
-    variables: { itemId: "string" }
-  });
-  expect(response.data.scanLineItem).toBe(true);
+  expect(response.data.lineItem).toEqual<LineItem>({ id: "string" });
 });
