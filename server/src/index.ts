3015b9faced9b7608420a6acce40c0a3a59663f7
kristofferpetersen <kp@morningtrain.dk>
2019-12-01 21:53:29 +0100

    add express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('asdf');
+import {apolloServer as apolloServerPromise} from "./createApolloServer";
+import express from "express";
+
+(async function(){
+  const app = express();
+  const path = '/graphql';
+
+  let apolloServer = await apolloServerPromise;
+  (apolloServer).applyMiddleware({ app,path });
+
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${apolloServer.graphqlPath}`)
+  );
+
+})();
