1d4868c82bac55db58f0e5545e7bd1e1b6f15e80
kristofferpetersen <kp@morningtrain.dk>
2019-12-02 02:16:19 +0100

    cool scroll nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,75 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { mount, route } from "navi";
+import { Router } from "react-navi";
+import { View } from "react-navi";
+import HelmetProvider from "react-navi-helmet";
+const mapRange = (
+  in_min: number,
+  in_max: number | undefined,
+  out_min: number,
+  out_max: number,
+  value: number | undefined
+) => {
+  return in_max != undefined && value != undefined
+    ? ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    : undefined;
+};
+
+function NewComponent() {
+  const [openPercent, set_openPercent] = useState<number>(1);
+  const ref = useRef<HTMLDivElement>(null);
+  const initPos = useRef<number | undefined>(undefined);
+  let mapRange1 = () =>
+    mapRange(
+      8,
+      initPos.current,
+      0,
+      1,
+      ref.current?.getBoundingClientRect().top
+    ) ?? 1;
+  useEffect(() => {
+    initPos.current = ref.current?.getBoundingClientRect().top;
+    set_openPercent(mapRange1());
+  }, []);
+  return (
+    <div
+      className="App"
+      onScroll={() => {
+        set_openPercent(mapRange1());
+      }}
+    >
+      <div
+        className="Items"
+        style={{
+          opacity: mapRange(0, 1, 0.7, 1, openPercent),
+          transformOrigin: "50% 150px",
+          transform: `scale(${mapRange(0, 1, 0.97, 1, openPercent)})`
+        }}
+      >
+        <div className="Header-Row">Items</div>
+      </div>
+      <div className="Settings" ref={ref}>
+        <div className="Header-Row">Settings</div>
+      </div>
+    </div>
+  );
+}
+
+const routes = mount({
+  "/": route({
+    title: "My Shop",
+    view: <NewComponent />
+  })
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Router routes={routes}>
+        <View />
+      </Router>
+    </HelmetProvider>
   );
 };
 
