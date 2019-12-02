f97fd8cefa0fc9159ff9cf9f746ee2e06069a803
kristofferpetersen <kp@morningtrain.dk>
2019-12-02 19:44:10 +0100

    add scanner page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,23 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import "./App.css";
 import { mount, route } from "navi";
-import { Router } from "react-navi";
-import { View } from "react-navi";
+import { Router, View } from "react-navi";
 import HelmetProvider from "react-navi-helmet";
-const mapRange = (
-  in_min: number,
-  in_max: number | undefined,
-  out_min: number,
-  out_max: number,
-  value: number | undefined
-) => {
-  return in_max != undefined && value != undefined
-    ? ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-    : undefined;
-};
+import { ScrollTestPage } from "./pages/ScrollTest";
 
-function NewComponent() {
-  const [openPercent, set_openPercent] = useState<number>(1);
-  const ref = useRef<HTMLDivElement>(null);
-  const initPos = useRef<number | undefined>(undefined);
-  let mapRange1 = () =>
-    mapRange(
-      8,
-      initPos.current,
-      0,
-      1,
-      ref.current?.getBoundingClientRect().top
-    ) ?? 1;
-  useEffect(() => {
-    initPos.current = ref.current?.getBoundingClientRect().top;
-    set_openPercent(mapRange1());
-  }, []);
-  return (
-    <div
-      className="App"
-      onScroll={() => {
-        set_openPercent(mapRange1());
-      }}
-    >
-      <div
-        className="Items"
-        style={{
-          opacity: mapRange(0, 1, 0.7, 1, openPercent),
-          transformOrigin: "50% 150px",
-          transform: `scale(${mapRange(0, 1, 0.97, 1, openPercent)})`
-        }}
-      >
-        <div className="Header-Row">Items</div>
-      </div>
-      <div className="Settings" ref={ref}>
-        <div className="Header-Row">Settings</div>
-      </div>
-    </div>
-  );
-}
+type ScannerPageProps = {};
+const ScannerPage: React.FC<ScannerPageProps> = () => {
+  return <div className="ScannerPage">Scanner</div>;
+};
 
 const routes = mount({
   "/": route({
     title: "My Shop",
-    view: <NewComponent />
+    view: <ScrollTestPage />
+  }),
+  scan: route({
+    title: "Scanner",
+    view: <ScannerPage />
   })
 });
 
